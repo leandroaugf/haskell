726ae5d47775d03180100fe0551de0adcf1ce017
leandroaugf <leandroaugf@gmail.com>
2026-05-01 22:56:00 -0300

    haskell tuples

--- 2-FlowControl/A03.hs
+++ 2-FlowControl/A03.hs
@@ -7,9 +7,16 @@ splitBill total people =
 
 main :: IO ()
 main = do
+    let cart = [ ("coffee", 25.0),
+                 ("detergent", 3.50),
+                 ("basketball", 90.0) ]
+
     let (each, tip) = splitBill 120.0 3
+    let names = map fst cart
+
     putStrLn ("each pays R$" ++ show each) 
     putStrLn ("Tip: R$" ++ show tip)
 
     let result = splitBill 130.0 3 
     print result
+    print names
