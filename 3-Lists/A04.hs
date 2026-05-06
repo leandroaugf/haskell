64959cf893fc12cd07c54a2f53775dea33999a18
leandroaugf <leandroaugf@gmail.com>
2026-05-05 21:27:09 -0300

    strings operations

--- 3-Lists/A04.hs
+++ 3-Lists/A04.hs
@@ -1,5 +1,23 @@
+import Data.Char (ord, chr, isAlpha)
+
+describeStart :: String -> String
+describeStart [] = "Empty String"
+describeStart (x:xs) =
+    "Starts with the letter '" 
+    ++ [x] 
+    ++ "' and has "
+    ++ show (length xs)
+    ++ " more chars"
+
+
 main :: IO ()
 main = do
-    let greeting = "hello"
-    let checkGreeting = (greeting == ['h','e','l','l','o'])
-    print checkGreeting
+    putStrLn $ describeStart "Haskell"
+    putStrLn $ describeStart ""
+    putStrLn $ describeStart "Lists"
+
+    putStrLn ("numeric character of letter a: " ++ show (ord 'a'))
+    putStrLn ("alpha character of number 97: " ++ print (chr 97))
+
+    print (ord 'a')
+    print (chr 97)
